1bdb3213b37f87381ae8b43ceb8ae68dc206ec71
Gustavo <118356161+torneira@users.noreply.github.com>
2024-10-22 07:38:56 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,9 +55,7 @@ function App() {
         usuarios.map(user=>{
           return(
             <div key={user.id} className='usuario-item'>
-              <h1>
-                {user.name}
-              </h1>
+              <h1>{user.name}</h1>
               <p>{user.email}</p>
               <p>{user.created_at}</p>
               <p>{user.updated_at}</p>
