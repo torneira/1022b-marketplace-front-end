2b6e1eb5af50ab5edd0bae81db499000a02d3d0b
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-01-20 07:48:47 -0400

    Atualização do css e código de excluir produto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 type ProdutoType = {
@@ -16,6 +17,21 @@ function App() {
     .then(resposta=>resposta.json())
     .then(dados=>setProdutos(dados))
   },[])
+  function handleExcluir(id:number){
+    fetch(`http://localhost:8000/produtos/${id}`,{
+      method:"DELETE"
+    })
+    .then(resposta=>{
+      if(resposta.status==200){
+        alert("Excluído com sucesso")
+        window.location.reload()
+      }
+      else{
+        alert("Erro ao excluir")
+      }
+    })
+  }
+
   return (
     <>  
       <div className="container-produtos">
@@ -26,6 +42,8 @@ function App() {
               <img src={prod.imagem} alt="Imagem de celular" />
               <p>{prod.preco}</p>
               <p>{prod.descricao}</p>
+              <button onClick={()=>{handleExcluir(prod.id)}}>Excluir</button>
+              <Link to="">Alterar</Link>
             </div>
           )
         })}
