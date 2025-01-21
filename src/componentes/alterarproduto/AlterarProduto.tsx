8dddc3f974a0ce3d039b3c4f57db7eb0ae399e3d
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-01-21 07:42:06 -0400

    Alterar produto

--- src/componentes/alterarproduto/AlterarProduto.tsx
+++ src/componentes/alterarproduto/AlterarProduto.tsx
@@ -1,5 +1,83 @@
+import { useParams } from "react-router-dom";
+import { FormEvent, useState, ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
 function AlterarProduto(){
-    return <div>Alterar Produto</div>
+    const {id} = useParams()
+    const navigate = useNavigate();
+        const [descricao,setDescricao] = useState("")
+        const [nome,setNome] = useState("")
+        const [preco,setPreco] = useState("")
+        const [imagem,setImagem] = useState("")
+    
+        function handleForm(event:FormEvent){
+            event.preventDefault();
+            console.log("Tentei cadastrar produtos");
+            const produto = {
+                id: id,
+                nome: nome,
+                descricao: descricao,
+                preco: preco,
+                imagem: imagem
+            }
+            fetch("http://localhost:8000/produtos",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(produto)
+            }).then(response => {
+                if(response.status === 200){
+                    alert("Produto cadastrado com sucesso")
+                    navigate("/")
+                }
+                else{
+                    alert("Erro ao cadastrar produto")
+                }
+            })
+        }
+        function handleDescricao(event:ChangeEvent<HTMLInputElement>){
+            setDescricao(event.target.value)
+        }
+        function handlePreco(event:ChangeEvent<HTMLInputElement>){
+            setPreco(event.target.value)
+        }
+        function handleNome(event:ChangeEvent<HTMLInputElement>){
+            setNome(event.target.value)
+        }
+        function handleImagem(event:ChangeEvent<HTMLInputElement>){
+            setImagem(event.target.value)
+        }
+    
+    return (
+        <>
+            <div>Alterar Produto {id}</div>
+            <form onSubmit={handleForm}>
+                <div>
+                    <label htmlFor="id">id</label>
+                    <input type="text" name="id" value={id} readOnly />
+                </div>
+                <div>
+                    <label htmlFor="nome">nome</label>
+                    <input type="text" name="nome" onChange={handleNome} />
+                </div>
+                <div>
+                    <label htmlFor="descricao">descricao</label>
+                    <input type="text" name="descricao" onChange={handleDescricao} />
+                </div>
+                <div>
+                    <label htmlFor="preco">preço</label>
+                    <input type="text" name="preco" onChange={handlePreco} />
+                </div>
+                <div>
+                    <label htmlFor="imagem">imagem</label>
+                    <input type="text" name="imagem" onChange={handleImagem}/>
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar"/>
+                </div>
+            </form>
+        </>
+    )
 }
 
 export default AlterarProduto;
--- src/main.tsx
+++ src/main.tsx
@@ -8,17 +8,18 @@ import {
 } from "react-router-dom";
 import CadastroProduto from './componentes/cadastroproduto/CadastroProduto.tsx';
 import AlterarProduto from './componentes/alterarproduto/AlterarProduto.tsx';
+import Header from './componentes/header/Header.tsx';
 
 //Crie um comentário explicando o que faz o código abaixo
 //Cria uma instância do BrowserRouter e passa um array de objetos com as rotas da aplicação
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <><Header/><App /></>,
   },
   {
     path: "/cadastro-produto",
-    element: <CadastroProduto/>,
+    element: <><Header/><CadastroProduto/></>,
   },
   {
     path: "/alterar-produto/:id",
